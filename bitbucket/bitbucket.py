47481d7ec0dacf9b1b032088f65e9515cd6fa1af
Konstantin Molchanov <moigagoo@live.com>
2014-11-18 00:55:39 +0300

    Docstrings improved for all validators.

--- bitbucket/bitbucket.py
+++ bitbucket/bitbucket.py
@@ -1,4 +1,4 @@
-﻿'''Bitbucket Sloth CI validator that validates the Bitbucket payload against username and repo name (obtained from the Sloth app config).'''
+﻿'''Bitbucket Sloth CI validator that validates the `Bitbucket <https://bitbucket.org/>`_ payload against username and repo name (obtained from the Sloth app config).'''
 
 
 __title__ = 'sloth-ci.validators.bitbucket'
