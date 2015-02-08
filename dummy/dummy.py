d8a5073abd2791a5574f926e6fffeaf06b63cf3b
Konstantin Molchanov <moigagoo@live.com>
2015-02-08 23:40:55 +0300

    Dummy: 1.0.4: Updated for Sloth CI 1.1.9.

--- dummy/dummy.py
+++ dummy/dummy.py
@@ -10,7 +10,7 @@ A validator must implement the ``validate(request, validation_data)`` function. 
 
 __title__ = 'sloth-ci.validators.dummy'
 __description__ = 'Dummy validator for Sloth CI'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
@@ -21,17 +21,18 @@ def validate(request, validation_data):
 
     :param request: request with payload to validate
     :param validation_data: dictionary with the key ``message``
-    :returns: (True, success message, extracted data dict) if the payload is valid, (False, error message, extracted data dict) otherwise
+    :returns: (status, message, list of extracted param dicts)
     '''
 
     if request.method != 'GET':
-        return (405, 'Payload validation failed: Wrong method, GET expected, got {method}.', {'method': request.method})
+        return (405, 'Payload validation failed: Wrong method, GET expected, got %s.' % request.method, [])
 
     message = request.params.get('message')
 
     valid_message = validation_data.get('message')
 
     if message and message == valid_message:
-        return (200, 'Payload validated. Message: {message}', {'message': message})
+        return (200, 'Payload validated. Message: %s' % message, [{'message': message}])
+
     else:
-        return (403, 'Payload validation failed. Message: {message}', {'message': message})
+        return (403, 'Payload validation failed. Message: %s' % message, [])
