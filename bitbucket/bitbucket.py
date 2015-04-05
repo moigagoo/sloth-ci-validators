19bd23d826ccf8b5e4766f4ff270ac64c3968048
Konstantin Molchanov <moigagoo@live.com>
2015-04-05 14:55:01 +0300

    Bitbucket: 1.0.6: Branch validation added.

--- bitbucket/bitbucket.py
+++ bitbucket/bitbucket.py
@@ -1,9 +1,27 @@
-﻿'''Bitbucket Sloth CI validator that validates the `Bitbucket <https://bitbucket.org/>`_ payload against username and repo name (obtained from the Sloth app config).'''
+﻿'''Bitbucket Sloth CI validator that validates the `Bitbucket <https://bitbucket.org/>`_ payload against username and repo name (obtained from the Sloth app config).
+
+Usage in the app config::
+
+    provider:
+        bitbucket:
+            # Repository owner. Mandatory parameter.
+            owner: moigagoo
+
+            # Repository title as it appears in the URL, i.e. slug.
+            # Mandatory parameter.
+            repo: sloth-ci
+
+            # Only pushes to these branches will initiate a build.
+            # Skip this parameter to allow all branches to fire builds.
+            branches:
+                - master
+                - staging
+'''
 
 
 __title__ = 'sloth-ci.validators.bitbucket'
 __description__ = 'Bitbucket validator for Sloth CI'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
@@ -13,7 +31,7 @@ def validate(request, validation_data):
     '''Validate Bitbucket payload against repo name (obtained from the Sloth app config).
 
     :param request_params: payload to validate
-    :param validation_data: dictionary with the key ``repo`` (in the form "username/repo")
+    :param validation_data: dictionary with the keys ``owner``, ``repo``, and ``branches``
 
     :returns: (status, message, list of extracted param dicts)
     '''
@@ -35,17 +53,24 @@ def validate(request, validation_data):
 
         parsed_payload = loads(payload)
 
-        repo = '{owner}/{slug}'.format(
-            owner=parsed_payload['repository']['owner'],
-            slug=parsed_payload['repository']['slug']
-        )
+        owner = parsed_payload['repository']['owner']
+
+        if owner != validation_data['owner']:
+            return (403, 'Payload validation failed: wrong owner: %s' % owner, [])
+
+        repo = parsed_payload['repository']['slug']
 
         if repo != validation_data['repo']:
-            return (403, 'Payload validation failed: repo mismatch. Repo: %s' % repo, [])
+            return (403, 'Payload validation failed: wrong repository: %s' % repo, [])
 
         branches = {commit['branch'] for commit in parsed_payload['commits']}
 
-        param_dicts = [{'branch': branch} for branch in branches]
+        allowed_branches = set(validation_data.get('branches', branches))
+
+        if not branches & allowed_branches:
+            return (403, 'Payload validation failed: wrong branches: %s' % branches, [])
+
+        param_dicts = [{'branch': branch} for branch in branches & allowed_branches]
 
         return (200, 'Payload validated. Branches: %s' % ', '.join(branches), param_dicts)
 
