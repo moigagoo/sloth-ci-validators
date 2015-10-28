2d12eb68406d1d095cb95a921a973159f150a042
Konstantin Molchanov <moigagoo@live.com>
2015-10-28 10:19:34 +0300

    GitHub: 1.0.9: Namedtuples are now returned instead of plain tuples.

--- github/github.py
+++ github/github.py
@@ -21,7 +21,7 @@ Usage in the app config::
 
 __title__ = 'sloth-ci.validators.github'
 __description__ = 'GitHub validator for Sloth CI'
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
@@ -30,23 +30,29 @@ __license__ = 'MIT'
 def validate(request, validation_data):
     '''Check payload from GitHub: the origin IP must be genuine; the repo owner and title must be valid.
 
-    :param request_params: `CherryPy request <http://docs.cherrypy.org/en/latest/pkg/cherrypy.html#cherrypy._cprequest.Request>`_ object instance representing the incoming request
-    :param validation_data: dictionary with the keys ``owner``, ``repo``, and ``branches``, parsed from the config
+    :param request_params: `CherryPy request <http://docs.cherrypy.org/en/latest/pkg/cherrypy.ht
+       ml#cherrypy._cprequest.Request>`_ instance representing incoming request
+    :param validation_data: dict with the keys ``owner``, ``repo``, and ``branches``, parsed from the config
 
-    :returns: (status, message, list of extracted param dicts)
+    :returns: namedtuple(status, message, list of extracted params as dicts), e.g. ``Response(status=200, message='Payload validated. Branches: default', [{'branch': 'default'}])``
     '''
+
+    from collections import namedtuple
 
     from ipaddress import ip_address, ip_network
 
+
+    response = namedtuple('Response', ('status', 'message', 'param_dicts'))
+
     if request.method != 'POST':
-        return (405, 'Payload validation failed: Wrong method, POST expected, got %s.' % request.method, [])
+        return response(405, 'Payload validation failed: Wrong method, POST expected, got %s.' % request.method, [])
 
     trusted_ips = ip_network('192.30.252.0/22')
 
     remote_ip = ip_address(request.remote.ip)
 
     if remote_ip not in trusted_ips:
-        return (403, 'Payload validation failed: Unverified remote IP: %s.' % remote_ip, [])
+        return response(403, 'Payload validation failed: Unverified remote IP: %s.' % remote_ip, [])
 
     try:
         payload = request.json
@@ -59,24 +65,24 @@ def validate(request, validation_data):
             owner = payload['repository']['owner']['name']
 
         if owner != validation_data['owner']:
-            return (403, 'Payload validation failed: wrong owner: %s' % owner, [])
+            return response(403, 'Payload validation failed: wrong owner: %s' % owner, [])
 
         repo = payload['repository']['name']
 
         if repo != validation_data['repo']:
-            return (403, 'Payload validation failed: wrong repository: %s' % repo, [])
+            return response(403, 'Payload validation failed: wrong repository: %s' % repo, [])
 
         if is_ping:
-            return (200, 'Ping payload validated', [])
+            return response(200, 'Ping payload validated', [])
 
         branch = {payload['ref'].split('/')[-1]}
 
         allowed_branches = set(validation_data.get('branches', branch))
 
         if not branch & allowed_branches:
-            return (403, 'Payload validation failed: wrong branch: %s' % branch, [])
+            return response(403, 'Payload validation failed: wrong branch: %s' % branch, [])
 
-        return (200, 'Payload validated. Branch: %s' % branch, [{'branch': branch}])
+        return response(200, 'Payload validated. Branch: %s' % branch, [{'branch': branch}])
 
     except Exception as e:
-        return (400, 'Payload validation failed: %s' % e, [])
+        return response(400, 'Payload validation failed: %s' % e, [])
