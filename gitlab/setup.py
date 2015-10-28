5c4055e4845ebe0695815fc47b02109c9e87f0d3
Konstantin Molchanov <moigagoo@live.com>
2015-10-28 10:32:40 +0300

    GitLab: 1.0.1: IP check added; updated for Sloth CI 2.x; namedtuples are returned instead of plain tuples.

--- gitlab/setup.py
+++ gitlab/setup.py
@@ -1,4 +1,4 @@
-﻿from setuptools import setup
+from setuptools import setup
 
 import gitlab as validator
 
@@ -17,7 +17,7 @@ setup(
     packages=[package],
     package_dir={package: '.'},
     install_requires = [
-        'sloth-ci>=1.1.9'
+        'sloth-ci>=2.0.1'
     ],
     license=validator.__license__,
     classifiers=[
